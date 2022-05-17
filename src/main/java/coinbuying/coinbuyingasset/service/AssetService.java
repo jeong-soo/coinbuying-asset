dee40d77c2d4a7952f3235f511a67f7412f4b200
huisoo <huiwater@gmail.com>
2022-05-17 18:34:29 +0900

    api : upbitWallet 처리 중

--- src/main/java/coinbuying/coinbuyingasset/service/AssetService.java
+++ src/main/java/coinbuying/coinbuyingasset/service/AssetService.java
@@ -2,11 +2,13 @@ package coinbuying.coinbuyingasset.service;
 
 import coinbuying.coinbuyingasset.dto.response.UserAssetResponse;
 import coinbuying.coinbuyingasset.entity.UserAsset;
+import org.json.simple.JSONObject;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface AssetService {
+
     Mono<UserAssetResponse> getWallet(ServerRequest serverRequest);
-    Mono<Object> getPostsByBoardType(ServerRequest serverRequest);
+    public String getUpbitWallet(ServerRequest serverRequest);
 }
--- src/main/java/coinbuying/coinbuyingasset/service/AssetServiceImpl.java
+++ src/main/java/coinbuying/coinbuyingasset/service/AssetServiceImpl.java
@@ -5,15 +5,37 @@ import coinbuying.coinbuyingasset.dto.response.UserAssetResponse;
 
 import coinbuying.coinbuyingasset.repository.R2dbcEntityAssetRepository;
 import coinbuying.coinbuyingasset.repository.UserAssetRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 
 import reactor.core.publisher.Mono;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 
 @Service
@@ -41,7 +63,53 @@ public class AssetServiceImpl implements AssetService {
     }
 
     @Override
-    public Mono<Object> getPostsByBoardType(ServerRequest serverRequest) {
-        return null;
+    public String getUpbitWallet(ServerRequest serverRequest) {
+
+        String accessKey = "sZiMJou0evyRRV3GB6Nrtgo1a9fuEU5OnSjBHRqM";//System.getenv("UPBIT_OPEN_API_ACCESS_KEY");
+        String secretKey = "898Usfgbf54lJDiVs7nBL3mHxLElrBoZRAPnW7dx";//System.getenv("UPBIT_OPEN_API_SECRET_KEY");
+        String serverUrl = "https://api.upbit.com";//System.getenv("UPBIT_OPEN_API_SERVER_URL");
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        String jwtToken = JWT.create()
+                .withClaim("access_key", accessKey)
+                .withClaim("nonce", UUID.randomUUID().toString())
+                .sign(algorithm);
+
+        String authenticationToken = "Bearer " + jwtToken;
+
+        JSONObject jsonObj = null;
+        String retVal = "";
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(serverUrl + "/v1/accounts");
+            request.setHeader("Content-Type", "application/json");
+            request.addHeader("Authorization", authenticationToken);
+
+            HttpResponse response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            String upbitWalletArray =  EntityUtils.toString(entity, "UTF-8");
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<Object> upbitWalletList = objectMapper.readValue(upbitWalletArray, new TypeReference<>() {});
+
+            for(int i=0; i<upbitWalletList.size();i++){
+                Map<String, Object> map = (Map<String,Object>) upbitWalletList.get(i);
+                System.out.println(map.get("currency").toString());
+                System.out.println(Double.parseDouble(map.get("balance").toString()));
+                System.out.println(Double.parseDouble(map.get("locked").toString()));
+                System.out.println(Integer.parseInt(map.get("avg_buy_price").toString()));
+                System.out.println(Boolean.parseBoolean(map.get("avg_buy_price_modified").toString()));
+                System.out.println(map.get("unit_currency").toString());
+
+            }
+
+            System.out.println(EntityUtils.toString(entity, "UTF-8"));
+
+            retVal = EntityUtils.toString(entity, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+
+        return retVal;
     }
 }
